57b0644f3573b2c8b19bb0bd7554a5b193eee649
Joshua Himmens <joshua.himmens@gmail.com>
2021-05-27 19:43:34 -0600

    setup

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import numpy
 import math
-
-
+import time
+pp = numpy.pi
 def pi(limit=1000000):
+    start = time.time()
     c = 4
     n = c
     a = 1
     d = -1
 
-    while abs(n) != numpy.pi and d < limit:
+    while abs(n) != pp:
         n += a * (4/d)
         d += 2
+        if d % 30000000 == 1:
+            t = f'{abs(n):.15f}'
+            print(f'working with: {t}\t{d}')
+            print(f'What we need: {pp}')
         if a == 1:
             a = -1
         elif a == -1:
@@ -19,6 +24,8 @@ def pi(limit=1000000):
             print("ERROR")
             break
     n = n * -1
+    finish = time.time() - start
+    print(finish)
     print(f'{n}\n{numpy.pi}\n{math.pi}')
     return n
 
