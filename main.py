b1f037207e666cd7cc499a6831203c472ceb60dc
Joshua Himmens <joshua.himmens@gmail.com>
2021-05-29 01:45:42 -0600

    active

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy
 import math
 import time
 import twilio.rest as twil
+import sys
 
 sid = "ACee46d15aee42ece35152b01a0cf0db61"
 tokensid = 'a16dc9df73edf829357d5354e5802147'
@@ -42,19 +43,78 @@ def pi(limit=100000000000000):
     return n, finish
 
 
-def pi2(limit):
+def pi2(limit=10000000):
     num = 2
     den = 1
-    final = 1
+    final = 2.
     flip = 0
-    for i in limit:
-        print(i)
+    i = 0
+    while (final != numpy.pi) and (i <= limit):
+        i += 1
+        if i % 10000000 == 0:
+            print(f'working with: {str(final):.17}\t{i}')
+            print(f'What we need: {pp}')
         final = final * (num/den)
         if flip % 2 == 0:
-            den += 1
+            den += 2
+            flip = 1
         elif flip % 2 == 1:
-            num += 1
-    print(final)
+            num += 2
+            flip = 0
+    print(i)
 
 
-pi2(2)
+def pi1time(timelimit=30):
+    start = time.time()
+    c = 4
+    n = c
+    a = 1
+    d = -1
+    while time.time() - start <= timelimit:
+        n += a * (4 / d)
+        d += 2
+        if (d % 100000000 == 1) and False:
+            t = f'{abs(n):.15f}'
+            print(f'working with: {t}\t{d}')
+            print(f'What we need: {pp}')
+        if a == 1:
+            a = -1
+        elif a == -1:
+            a = 1
+        else:
+            print("ERROR")
+            break
+    n = n * -1
+    finish = time.time() - start
+    print('p1:', finish)
+    print(f'p1: {n}\nEx: {numpy.pi}')
+    return n, finish
+
+
+def pi2time(timelimit=30):
+    start = time.time()
+    num = 2
+    den = 1
+    final = 2.
+    flip = 0
+    i = 0
+    while time.time() - start <= timelimit:
+        i += 1
+        if (i % 10000000 == 0) and False:
+            print(f'working with: {str(final):.17}\t{i}')
+            print(f'What we need: {pp}')
+        final = final * (num / den)
+        if flip % 2 == 0:
+            den += 2
+            flip = 1
+        elif flip % 2 == 1:
+            num += 2
+            flip = 0
+    finish = time.time() - start
+    print('p2:', finish)
+    print(f'p2: {final}\nEx: {numpy.pi}')
+
+
+pi1time(timelimit=test_time)
+pi2time(timelimit=test_time)
+
