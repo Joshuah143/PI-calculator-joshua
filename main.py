bbb40cdd73d2624c129913f69f8c906d75a7a51b
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-06-02 22:20:45 -0600

    Finish

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy
 import math
 import time
 import twilio.rest as twil
-import sys
+
 
 sid = "ACee46d15aee42ece35152b01a0cf0db61"
 tokensid = 'a16dc9df73edf829357d5354e5802147'
@@ -17,6 +17,7 @@ def send_text(message):
 
 
 def pi(limit=100000000000000):
+    # very long, infinite
     start = time.time()
     c = 4
     n = c
@@ -44,6 +45,7 @@ def pi(limit=100000000000000):
 
 
 def pi2(limit=10000000):
+    # very long, infintit
     num = 2
     den = 1
     final = 2.
@@ -61,80 +63,35 @@ def pi2(limit=10000000):
         elif flip % 2 == 1:
             num += 2
             flip = 0
-    print(i)
+    return i
 
 
-def pi3(n=10000000000000000000000000000000000000000000000000000):
+def pi3(n=65001314):
+    # very short, finite
     n = int(n)
     d = 360/n
     rd = (180-d)/2
     r = 1
     diamiter = 2
-    e = (r * float(numpy.sin(d * numpy.pi / 180.)))/float(numpy.sin(rd* numpy.pi / 180.))
+    e = (r * float(numpy.sin(d * numpy.pi / 180.)))/(float(numpy.sin(rd * numpy.pi / 180.)))
     circum = e * n
     pivalue = circum / diamiter
     return pivalue
 
 
-def pi1time(timelimit=30):
-    start = time.time()
-    c = 4
-    n = c
-    a = 1
-    d = -1
-    while time.time() - start <= timelimit:
-        n += a * (4 / d)
-        d += 2
-        if (d % 100000000 == 1) and False:
-            t = f'{abs(n):.15f}'
-            print(f'working with: {t}\t{d}')
-            print(f'What we need: {pp}')
-        if a == 1:
-            a = -1
-        elif a == -1:
-            a = 1
-        else:
-            print("ERROR")
-            break
-    n = n * -1
-    finish = time.time() - start
-    print('p1:', finish)
-    print(f'p1: {n}\nEx: {numpy.pi}')
-    return n, finish
+def pi4(increment=0.00005):
+    # finite, inacurate, no pi, pure python
+    ycalc = lambda a: ((1 - (a ** 2)) ** 0.5)
+    pythag = lambda y, yn, x, xn: (abs((((abs(yn - y)) ** 2) + ((abs(xn - x)) ** 2)) ** 0.5))
+    increment = float(increment)
+    xvalue = increment
+    lastxvalue = 0
+    total_length = 0
+    while xvalue <= 1:
+        total_length += pythag(ycalc(lastxvalue), ycalc(xvalue), lastxvalue, xvalue)
+        lastxvalue = xvalue
+        xvalue += increment
+    total_length = total_length * 4
+    pi_value = total_length/2
+    return pi_value
 
-
-def pi2time(timelimit=30):
-    start = time.time()
-    num = 2
-    den = 1
-    final = 2.
-    flip = 0
-    i = 0
-    while time.time() - start <= timelimit:
-        i += 1
-        if (i % 10000000 == 0) and False:
-            print(f'working with: {str(final):.17}\t{i}')
-            print(f'What we need: {pp}')
-        final = final * (num / den)
-        if flip % 2 == 0:
-            den += 2
-            flip = 1
-        elif flip % 2 == 1:
-            num += 2
-            flip = 0
-    finish = time.time() - start
-    print('p2:', finish)
-    print(f'p2: {final}\nEx: {numpy.pi}')
-
-
-if __name__ == '__ma in__':
-    test_time = 30
-    pi1time(timelimit=test_time)
-    pi2time(timelimit=test_time)
-
-starttime = time.time()
-calculated = float(pi3(n=10000000000000000000000000000000000000000000000000000))
-fin = starttime - time.time()
-print(f'{calculated:.30}')
-print(f'{numpy.pi:.30}')
-print(fin)
